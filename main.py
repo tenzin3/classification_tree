83777b2de0210298fc417e2ec5408dfc4688226a
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-08-04 11:15:26 +0000

    Classifier code skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+class ClassificationTree:
+    def __init__(self):
+        pass 
+
+    @property
+    def training_res(self):
+        pass 
+
+    def train(self, feature: dict[str, list[float]], label: list[int]):
+        pass 
+
+    def predict(self, feature: list[float]):
+        pass 
+
+
+
+
