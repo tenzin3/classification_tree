6fd543bd41dd9916325634b8e22b2566209fcf9e
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-08-04 11:34:34 +0000

    modularize validate training data process

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Classifier:
     def training_res(self):
         pass 
 
-    def train(self, feature: dict[str, list[int | float]], label: list[int]):
+    def _validate_training_data(self, feature: dict[str, list[int | float]], label: list[int]):
         # data validation
         label_count = len(label)
 
@@ -15,7 +15,11 @@ class Classifier:
             if val_count != label_count:
                 raise ValueError(f"Feature {feat} values count is not equal to label count.")
             
-            
+        
+    def _calculate_accuracy(self):
+        pass 
+
+    def train(self, feature: dict[str, list[int | float]], label: list[int]):
         pass 
 
     def predict(self, feature: list[float]):
