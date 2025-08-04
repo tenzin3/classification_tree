145e25120f3acfda40a84dfe6f529cde445ab71f
tenzin3 <tenzintsunduebhattu@gmail.com>
2025-08-04 11:23:01 +0000

    data validation before training

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ class Classifier:
     def training_res(self):
         pass 
 
-    def train(self, feature: dict[str, list[float]], label: list[int]):
+    def train(self, feature: dict[str, list[int | float]], label: list[int]):
+        # data validation
+        label_count = len(label)
+
+        for feat, vals in feature.items():
+            val_count = len(vals)
+            if val_count != label_count:
+                raise ValueError(f"Feature {feat} values count is not equal to label count.")
+            
+            
         pass 
 
     def predict(self, feature: list[float]):
--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from main import Classifier
 
 class TestClassifier(TestCase):
     def setUp(self):
@@ -6,7 +7,11 @@ class TestClassifier(TestCase):
 
     # Two Label Categories
     def test_one_feature(self):
-        pass 
+        feature = {"petal_length": [0.1, 0.2, 0.48, 0.5, 1]}
+        label = [0,0,0,1,1]
+        
+        classifier = Classifier()
+        classifier.train(feature=feature, label=label)
 
     def test_two_feature(self):
         pass 
